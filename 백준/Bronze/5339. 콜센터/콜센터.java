8a602d26eb7a19643f93f3dec5b381e8e9270d83
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-08-26 04:46:59 +0900

    [Bronze V] Title: 콜센터, Time: 72 ms, Memory: 11296 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,13 @@
+class Main {
+	public static void main(String ergs[]) {
+		System.out.println("     /~\\\n"
+				+ "    ( oo|\n"
+				+ "    _\\=/_\n"
+				+ "   /  _  \\\n"
+				+ "  //|/.\\|\\\\\n"
+				+ " ||  \\ /  ||\n"
+				+ "============\n"
+				+ "|          |\n"
+				+ "|          |\n"
+				+ "|          |");	}
+}
