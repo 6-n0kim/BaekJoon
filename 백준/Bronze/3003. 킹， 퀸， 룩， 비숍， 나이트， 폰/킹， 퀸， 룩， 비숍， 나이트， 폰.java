13f7033662c6032be0e31f6904fc30ceeef350ea
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-08-26 04:17:03 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 268 ms, Memory: 19188 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String ergs[]) {
+		Scanner sc = new Scanner(System.in);
+		int a = 1 - sc.nextInt();
+		int b = 1 - sc.nextInt();
+		int c = 2 - sc.nextInt();
+		int d = 2 - sc.nextInt();
+		int e = 2 - sc.nextInt();
+		int f = 8 - sc.nextInt();
+		sc.close();
+		System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + f);
+	}
+}
