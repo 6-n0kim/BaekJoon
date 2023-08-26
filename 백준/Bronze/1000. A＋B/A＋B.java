2e08657548dc76865328edc3e115ef90bfaed417
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-08-26 15:29:43 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ class Main{
+	 public static void main(String ergs []) {
+		 Scanner sc = new Scanner(System.in);
+		 int A = 0;
+		 int B = 0;
+		 
+		 
+		 A = sc.nextInt();
+		 B = sc.nextInt();
+		 
+		 System.out.println(A + B);
+	 }
+ }
