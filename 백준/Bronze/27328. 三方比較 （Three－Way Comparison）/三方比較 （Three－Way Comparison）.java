999c0306a43076b5a2f9af7750858cfb6c88d9df
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-08-29 20:57:28 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 76 ms, Memory: 11456 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		System.out.println(a<b ? -1 : a==b ? 0 : 1);
+	}
+}
