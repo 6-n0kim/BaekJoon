e73cc80391b108e08d2da18a4b983270cc521fb4
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-08-29 13:01:32 +0900

    [Bronze V] Title: 홍익대학교, Time: 80 ms, Memory: 11456 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int i = Integer.parseInt(br.readLine());
+		System.out.println(i-1946);
+	}
+}
