902d4f0003bff79c313e623ca56c5b6dc7995fd7
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-04 22:28:45 +0900

    [Bronze V] Title: A+B - 8, Time: 220 ms, Memory: 15516 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		for (int i = 0; i < a; i++) {
+			int[] arr = Arrays.asList(br.readLine().split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+			System.out.printf("Case #%d: %d + %d = %d\n", i + 1, arr[0], arr[1], arr[0] + arr[1]);
+		}						
+	}
+}
