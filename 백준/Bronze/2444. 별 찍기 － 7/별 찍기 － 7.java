857f0dc82cf76bf847b7d8b002d2672fdee2c26d
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-06 11:51:53 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 132 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,5 +1,5 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
 
@@ -23,6 +23,5 @@ public class Main {
 			sb.append("\n");
 		}
 		System.out.print(sb);
-	
 	}
 }
