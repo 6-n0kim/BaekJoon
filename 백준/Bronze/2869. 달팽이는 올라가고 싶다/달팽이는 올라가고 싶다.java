47dcdee700c473556a7f71067db9f7a1243eba99
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-08 15:07:12 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 224 ms, Memory: 18752 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+		public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int[] arr = Arrays.asList(br.readLine().split(" ")).stream().mapToInt(Integer::parseInt).toArray();
+		int day = (arr[2]-arr[1])/(arr[0]-arr[1]);
 		if ((arr[2]-arr[1])%(arr[0]-arr[1]) == 0) {
-			System.out.println((arr[2]-arr[1])/(arr[0]-arr[1]));
+			System.out.println(day);
 		}else {
-			System.out.println((arr[2]-arr[1])/(arr[0]-arr[1])+1);
+			System.out.println(day+1);
 		}
 	}
 }
