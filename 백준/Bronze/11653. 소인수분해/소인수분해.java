f353f798c428f33c71eb026163d21b41fe621408
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-08 18:03:11 +0900

    [Bronze I] Title: 소인수분해, Time: 116 ms, Memory: 11972 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -5,17 +5,18 @@ import java.io.InputStreamReader;
 public class Main {
 
 public static void main(String[] args) throws NumberFormatException, IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(br.readLine());
+		StringBuilder sb =new StringBuilder();
 		while(a>1) {
 			for (int i = 2; i <= a; i++) {
 				if (a % i==0) {
 					a/=i;
-					System.out.println(i);
+					sb.append(i).append("\n");
 					break;
 				}
 			}
 		}
+		System.out.println(sb);
 	}
 }
