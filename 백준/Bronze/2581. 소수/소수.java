093c7a68e3c31097215d8920ad38da0e26357024
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-08 17:54:09 +0900

    [Bronze II] Title: 소수, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,33 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+public static void main(String[] args) throws NumberFormatException, IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(br.readLine());
 		int b = Integer.parseInt(br.readLine());
-		int cnt = 0;
-		int sum = 0;
-		for (int i = a; i <= b; i++) {
-			int chk = 0;
-			for (int j = 1; j < i; j++) {
-				if (i % j == 0) {
-					chk++;
-				}
-			}
-			if (chk == 1) {
-				if (cnt == 0) {
-					cnt = i;
-				}
-				sum += i;
+		
+		boolean[] arr = new boolean[b+1];
+		arr[1] = true;
+		for(int i = 2; i <= Math.sqrt(b); i++) {
+			if(arr[i]) continue;
+			for(int j = i + i; j <= b; j += i) {
+				arr[j] = true;
 			}
 		}
-		if (cnt == 0) {
-			System.out.println(-1);
-		} else {
-			System.out.println(sum);
-			System.out.println(cnt);
+		
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for(int i = a; i <= b; i++) {
+			if(!arr[i]) {
+				sum += i;
+				min = Math.min(min, i);
+			}
+		}
+		
+		if(sum == 0) System.out.println(-1);
+		else {
+			System.out.println(sum+"\n"+min);
 		}
 	}
 }
