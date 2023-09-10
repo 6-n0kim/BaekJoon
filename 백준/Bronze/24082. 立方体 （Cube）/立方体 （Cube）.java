423f0a072ad7c30aa500a090c9ea32836305ac99
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-10 15:06:21 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 76 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println((int)Math.pow(n, 3));
+		
+	}
+}
