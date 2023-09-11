1aa90494132dbd287e56f1ea74fea31646a30b66
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-11 11:49:48 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+interface Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(1l*n*(n-1)/2);
+        System.out.println(2);
+    }
+}
