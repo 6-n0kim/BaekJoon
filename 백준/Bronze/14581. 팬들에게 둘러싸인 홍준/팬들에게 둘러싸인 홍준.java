0adc0e312ecd92b1321f46db7625a04c49e01587
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-14 21:03:06 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 92 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		System.out.println(":fan::fan::fan:\n" + ":fan::" + s + "::fan:\n" + ":fan::fan::fan:");
+	}
+}
