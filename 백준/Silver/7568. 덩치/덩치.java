6c41bccb33a359834255a79280eee4efd6440361
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-18 11:18:55 +0900

    [Silver V] Title: 덩치, Time: 80 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][2];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = 0; i < n; i++) {
+			int rank = 1;
+
+			for (int j = 0; j < n; j++) {
+				if (i == j) continue;
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					rank++;
+				}
+			}
+			sb.append(rank+" ");
+		}
+		System.out.println(sb);
+	}
+}
