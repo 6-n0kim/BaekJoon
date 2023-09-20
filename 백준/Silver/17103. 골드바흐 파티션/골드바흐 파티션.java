7b0f5427c943928ac7389becd6444d8a765cd5f3
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-20 16:48:44 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 216 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -7,15 +7,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int t = Integer.parseInt(br.readLine());
-		int testCase[] = new int[t];
-		int max = 0;
-		for (int i = 0; i < testCase.length; i++) {
-			testCase[i] = Integer.parseInt(br.readLine());
-			if (testCase[i] > max)
-				max = testCase[i];
-		}
-
-		boolean arr[] = new boolean[max + 1];
+		boolean arr[] = new boolean[1000001];
 
 		arr[0] = arr[1] = true;
 		for (int i = 2; i * i < arr.length; i++) {
@@ -26,15 +18,15 @@ public class Main {
 			}
 		}
 
-		for (int r = 0; r < testCase.length; r++) {
-			int n = testCase[r];
+		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
 			int cnt = 0;
-			for (int i = 2; i <= n/2; i++) {
-				if (!arr[i] && !arr[n-i]) {
+			for (int i = 2; i <= n / 2; i++) {
+				if (!arr[i] && !arr[n - i]) {
 					cnt++;
 				}
 			}
-			sb.append(cnt+"\n");
+			sb.append(cnt + "\n");
 		}
 		System.out.println(sb);
 	}
