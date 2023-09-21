465dc7ac0b2b49f041421ad167c511aadda7b232
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-21 17:19:22 +0900

    [Bronze I] Title: 성 지키기, Time: 140 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		String arr[][] = new String[n][m];
+		boolean row[] = new boolean[n];
+		boolean col[] = new boolean[m];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = br.readLine().split("");
+		}
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				if (!row[i] && arr[i][j].equals("X")) {
+					row[i] = true;
+				} if (!col[j] && arr[i][j].equals("X")) {
+					col[j] = true;
+				}
+			}
+		}
+		int cntR = 0;
+		int cntC = 0;
+		for (int i = 0; i < row.length; i++) {
+			if (!row[i]) {
+				cntR++;
+			}
+		}
+		for (int i = 0; i < col.length; i++) {
+			if (!col[i]) {
+				cntC++;
+			}
+		}
+		System.out.println(Math.max(cntR, cntC));
+	}
+}
