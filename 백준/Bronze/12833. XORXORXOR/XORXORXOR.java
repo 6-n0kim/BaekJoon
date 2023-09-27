2314c10a7f61def8835c9d4673b770c9e3fbe4de
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-09-27 14:47:49 +0900

    [Bronze I] Title: XORXORXOR, Time: 76 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int n1 = Integer.parseInt(st.nextToken());
+		int n2 = Integer.parseInt(st.nextToken());
+		int n3 = Integer.parseInt(st.nextToken());
+		if (n3 % 2 == 0) {
+			System.out.println(n1);
+		}
+		else {
+			System.out.println(n1^n2);
+		}
+	}
+}
