74998f790334b1fd933476a444a84930ed956496
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-10-13 10:18:28 +0900

    [Silver V] Title: 투명, Time: 96 ms, Memory: 13788 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int arr[][] = new int[101][101];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			for (int j = x1; j <= x2; j++) {
+				for (int j2 = y1; j2 <= y2; j2++) {
+					arr[j][j2]++;
+				}
+			}
+		}
+		int cnt = 0;
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = 1; j < arr[i].length; j++) {
+				if (arr[i][j] > m) {
+					cnt++;
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
+}
