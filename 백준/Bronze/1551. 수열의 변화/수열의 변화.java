1cdc4e9213cf7c6909b93e11e7059a5c1fca979f
김준영 <111862345+6-n0kim@users.noreply.github.com>
2023-10-24 14:16:12 +0900

    [Bronze I] Title: 수열의 변화, Time: 80 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Bronze/1551. 수열의 변화/수열의 변화.java
+++ 백준/Bronze/1551. 수열의 변화/수열의 변화.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,14 +8,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		st.nextToken();
+		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		String s = br.readLine();
-		sb.append(s+",");
-		int arr[] = Arrays.asList(s.split(",")).stream().mapToInt(Integer::parseInt).toArray();
+		String s[] = br.readLine().split(",");
+		int arr[] = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(s[i]);
+			sb.append(arr[i] + ",");
+		}
+
 		for (int i = 0; i < k; i++) {
 			sb.setLength(0);
-			int arr2[] = new int[arr.length - 1];
+			int arr2[] = new int[arr.length-1];
 			for (int j = 0; j < arr2.length; j++) {
 				arr2[j] = arr[j + 1] - arr[j];
 				sb.append(arr2[j] + ",");
