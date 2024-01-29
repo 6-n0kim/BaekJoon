14fff0e142bbdc55299aa722e8f6fbb2351114f8
김준영 <111862345+6-n0kim@users.noreply.github.com>
2024-01-29 15:48:12 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 84 ms, Memory: 11876 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            for (int j = 1; j <= 2*n-2*i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            for (int j = 1; j <= 2*n-2*i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
