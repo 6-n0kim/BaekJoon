225c3e30f8cba0b1d57b0f116241fedd8bebf0bc
김준영 <111862345+6-n0kim@users.noreply.github.com>
2024-02-16 15:25:01 +0900

    [Bronze III] Title: 저작권, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+public class Main {
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int I = Integer.parseInt(st.nextToken());
+		bw.write(String.valueOf(A*(I-1)+1));
+		bw.flush();
+		bw.close();
+	}
+}
