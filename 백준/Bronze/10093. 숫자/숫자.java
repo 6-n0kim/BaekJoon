62beca89163139a8913c39c1ddc2f9c600ca0a47
김준영 <111862345+6-n0kim@users.noreply.github.com>
2024-03-28 14:02:25 +0900

    [Bronze II] Title: 숫자, Time: 192 ms, Memory: 24944 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());;
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        if(a>b){
+            long tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if(a==b)
+            sb.append(b-a).append("\n");
+        else sb.append(b-a-1).append("\n");
+        for(long i=a+1;i<b;i++){
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
