4d264aef0886c21bbe55bc87c9348840fe621813
김준영 <111862345+6-n0kim@users.noreply.github.com>
2024-03-31 18:11:10 +0900

    [Bronze II] Title: 유학 금지, Time: 88 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Bronze/2789. 유학 금지/유학 금지.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		char[] word = "CAMBRIDGE".toCharArray();
+		String input = br.readLine();
+
+		for (int i = 0; i < word.length; i++) {
+			if (input.indexOf(word[i]) > -1) {
+				input = input.replace(String.valueOf(word[i]), "");
+			}
+		}
+		System.out.println(input);
+	}
+}
