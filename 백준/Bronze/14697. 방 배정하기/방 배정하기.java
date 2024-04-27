b8725e29c8ff53992fc154991647d920315812ba
김준영 <1090jun@naver.com>
2024-04-27 15:26:27 +0900

    [Bronze II] Title: 방 배정하기, Time: 100 ms, Memory: 11556 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.java
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -15,33 +15,13 @@ public class Main {
 
 		int answer = 0;
 
-		for (int i = 1; i <= (n / a); i++) {
-
-			if (a * i > n) {
-				break;
-			} else if (a * i == n) {
-				answer = 1;
-				break;
-			}
-
-			for (int j = 1; j <= (n / b); j++) {
-
-				if ((a * i) + (b * j) > n) {
-					break;
-				} else if ((a * i) + (b * j) == n) {
-					answer = 1;
-					break;
-				}
-
-				for (int j2 = 1; j2 <= (n / c); j2++) {
-
-					if ((a * i) + (b * j) + (c * j2) > n) {
-						break;
-					} else if ((a * i) + (b * j) + (c * j2) == n) {
+		for (int i = 0; i <= (n / a); i++) {
+			for (int j = 0; j <= (n / b); j++) {
+				for (int j2 = 0; j2 <= (n / c); j2++) {
+					if (a * i + b * j + c * j2 == n) {
 						answer = 1;
 						break;
 					}
-
 				}
 			}
 		}
