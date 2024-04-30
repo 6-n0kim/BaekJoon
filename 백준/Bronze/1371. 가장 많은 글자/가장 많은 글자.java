33ba16ade2ceb6249fc7c087f2cf8f219fd6d9bb
김준영 <1090jun@naver.com>
2024-04-30 17:07:32 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 80 ms, Memory: 12100 KB -BaekjoonHub

---
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //scanner 와 유사
+
+        int[] alpha = new int[26]; //알파벳 개수 배열
+        int max = 0;
+        String input; //EOF 처리를 위해 받을 문자열;; 이거땜에 엄청 헤맴
+        String str = "";
+
+
+        while ((input = br.readLine()) != null) {
+            str += input;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                alpha[str.charAt(i) - 'a']++;
+
+                if (alpha[str.charAt(i) - 'a'] > max) {
+                    max = alpha[str.charAt(i) - 'a'];
+                }
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (max == alpha[i]) {
+                System.out.print((char) (i + 'a'));
+            }
+        }
+
+
+    }
+}
