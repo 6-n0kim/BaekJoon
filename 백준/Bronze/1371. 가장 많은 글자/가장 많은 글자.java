295e93732cc5d00a2c60519c6714ac5202fa00a9
김준영 <1090jun@naver.com>
2024-04-30 17:10:59 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 80 ms, Memory: 12060 KB -BaekjoonHub

--- 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.java
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.java
@@ -6,28 +6,28 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //scanner 와 유사
         StringBuilder sb = new StringBuilder();
-        int[] alpha = new int[26]; //알파벳 개수 배열
+        int[] arr = new int[26]; //알파벳 개수 배열
         int max = 0;
-        String input; //EOF 처리를 위해 받을 문자열;; 이거땜에 엄청 헤맴
-        String str = "";
+        String as; //EOF 처리를 위해 받을 문자열;; 이거땜에 엄청 헤맴
+        String s = "";
 
 
-        while ((input = br.readLine()) != null) {
-            str += input;
+        while ((as = br.readLine()) != null) {
+            s += as;
         }
 
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != ' ') {
-                alpha[str.charAt(i) - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                arr[s.charAt(i) - 'a']++;
 
-                if (alpha[str.charAt(i) - 'a'] > max) {
-                    max = alpha[str.charAt(i) - 'a'];
+                if (arr[s.charAt(i) - 'a'] > max) {
+                    max = arr[s.charAt(i) - 'a'];
                 }
             }
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (max == alpha[i]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (max == arr[i]) {
                sb.append((char) (i + 'a'));
             }
         }
