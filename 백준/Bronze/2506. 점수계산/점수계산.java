97a9eed4eefe01926143e9d3067667559232c57b
김준영 <1090jun@naver.com>
2024-05-07 20:22:32 +0900

    [Bronze III] Title: 점수계산, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Bronze/2506. 점수계산/점수계산.java
+++ 백준/Bronze/2506. 점수계산/점수계산.java
@@ -1,26 +1,20 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int cont = 0;
-		int sum = 0;
-		for (int j = 0; j < n; j++) {
-			int m = Integer.parseInt(st.nextToken());
-			if (m == 1) {
-				cont++;
-			} else {
-				cont = 0;
-			}
-			sum += cont;
-		}
-		System.out.println(sum);
-
-	}
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    br.readLine();
+    int point = 1, sum = 0;
+    for (char c : br.readLine().toCharArray()) {
+      if (c == '0') {
+        point = 1;
+      } else if (c == '1') {
+        sum += point++;
+      }
+    }
+    System.out.println(sum);
+  }
 }
