f8da1782cb2fb9ab8b67dd9ee167ec4a3969a32c
김준영 <1090jun@naver.com>
2024-05-26 18:27:39 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 88 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String s = br.readLine();
+		
+		System.out.println(s.substring(n-5,n));
+	}
+}
