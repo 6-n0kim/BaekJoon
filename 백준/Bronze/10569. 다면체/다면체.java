ab6433719d8e8b0eeaeab4fbd4247155011af256
김준영 <1090jun@naver.com>
2024-06-24 13:21:45 +0900

    [Bronze III] Title: 다면체, Time: 72 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			System.out.println(2 - Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+		}
+	}
+}
