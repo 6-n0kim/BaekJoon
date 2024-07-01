93abd242f03b38ea2a851b7e4934bf15a91ccd81
김준영 <1090jun@naver.com>
2024-07-01 13:05:28 +0900

    [Bronze III] Title: 내 학점을 구해줘, Time: 68 ms, Memory: 12004 KB -BaekjoonHub

--- 백준/Bronze/10984. 내 학점을 구해줘/내 학점을 구해줘.java
+++ 백준/Bronze/10984. 내 학점을 구해줘/내 학점을 구해줘.java
@@ -10,19 +10,19 @@ public class Main {
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int i = 0; i < T; i++) {
-			int N = Integer.parseInt(br.readLine());
-			int Csum = 0;
-			double Gave = 0;
+			int n = Integer.parseInt(br.readLine());
+			int sum = 0;
+			double avg = 0;
 			
-			for(int j = 0; j < N; j++) {
+			for (int j = 0; j < n; j++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				int C = Integer.parseInt(st.nextToken());
-				double G = Double.parseDouble(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				double g = Double.parseDouble(st.nextToken());
 				
-				Csum += C;
-				Gave += (G * C);
+				sum += c;
+				avg += (g * c);
 			}
-			System.out.println(Csum + " " + String.format("%.1f", (Gave / Csum)));
+			System.out.println(sum + " " + String.format("%.1f", (avg / sum)));
 		}
 	}
 
