f5d5dbbd31025716102d72b7826c96d7a1ebb998
김준영 <1090jun@naver.com>
2024-07-17 13:08:06 +0900

    [Bronze V] Title: Everyone is a winner, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/31450. Everyone is a winner/Everyone is a winner.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		System.out.println(h % w == 0 ? "Yes" : "No");
+
+	}
+}
