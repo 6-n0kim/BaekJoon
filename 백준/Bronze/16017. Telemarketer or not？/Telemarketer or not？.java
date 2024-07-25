4c296c07cdb0597466556fd258f670f26a23d011
김준영 <1090jun@naver.com>
2024-07-25 16:05:43 +0900

    [Bronze IV] Title: Telemarketer or not?, Time: 76 ms, Memory: 11440 KB -BaekjoonHub

---
+++ 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+		int d = Integer.parseInt(br.readLine());
+
+		if (a >= 8 && b == c && d >= 8)
+			System.out.println("ignore");
+		else
+			System.out.println("answer");
+	}
+}
